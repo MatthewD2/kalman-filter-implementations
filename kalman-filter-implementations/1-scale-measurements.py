bb80f83e00e6e0bae461cdd80406fd562d965212
Matthew Devor <95507897+MatthewD2@users.noreply.github.com>
2024-07-29 09:44:52 -0500

    Add files via upload

---
+++ kalman-filter-implementations/1-scale-measurements.py
@@ -0,0 +1,32 @@
+#The Following Program is designed to estimate the exact weight of an object given several weight measurements
+#The weight of the object is treated as a random variable
+
+##### The 5 Kalman Filter Equations #####
+
+#State Update Equation
+
+#State Extrapolation Equation
+
+#Covariance Extrapolation Equation
+
+#Kalman Gain Equation
+
+#Covariance Update Equation
+
+
+
+##### Kalman Filter Step Functions #####
+
+#Initialization step
+
+#Measure Step
+
+#Update Step
+
+#Predict Step
+
+
+
+##### Driver Function #####
+
+#Define main()
